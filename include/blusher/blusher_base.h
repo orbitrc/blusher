29b8bb9660dd776b8eb26a9db5fecf9eabad867b
name <hardboiled65@gmail.com>
2020-03-27 06:12:34 +0900

    Platform macros added.

--- include/blusher/blusher_base.h
+++ include/blusher/blusher_base.h
@@ -9,4 +9,12 @@
     #define BL_EXPORT Q_DECL_IMPORT
 #endif
 
+#ifdef Q_OS_LINUX
+    #define BL_PLATFORM_LINUX
+#elif Q_OS_MACOS
+    #DEFINE BL_PLATFORM_MACOS
+#elif Q_OS_WIN
+    #DEFINE BL_PLATFORM_WINDOWS
+#endif
+
 #endif // _BL_BLUSHER_BASE_H
